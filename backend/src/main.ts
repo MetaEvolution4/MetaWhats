560fab1c0d0b56bd0b99fa3f79122c0b445c451c
Ricardo Matarazzo <ricardo54351060@gmail.com>
2026-06-14 16:37:22 -0300

    fix(backend): Add global prefix /api to NestJS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({ origin: process.env.CORS_ORIGIN || '*' });
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   
   const config = new DocumentBuilder()

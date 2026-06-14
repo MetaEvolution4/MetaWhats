619afcdaea2e99f650c8d2b33ddf911521b9e5a8
Ricardo Matarazzo <ricardo54351060@gmail.com>
2026-06-14 19:28:54 -0300

    fix(backend): Import PushModule in EventsModule to resolve Dependency Injection error

--- backend/src/events/events.module.ts
+++ backend/src/events/events.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { EventsGateway } from './events.gateway';
 import { JwtModule } from '@nestjs/jwt';
+import { PushModule } from '../push/push.module';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'super_secret_jwt_key_12345',
     }),
+    PushModule,
   ],
   providers: [EventsGateway],
   exports: [EventsGateway],

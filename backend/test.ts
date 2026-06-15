0de0aeaccac0f8e233ca06cf75e42baea203f2c6
Ricardo Matarazzo <ricardo54351060@gmail.com>
2026-06-14 22:36:38 -0300

    Fix build output directory by removing root test file

--- backend/test.ts
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-prisma.message.findMany({orderBy: {created_at: 'desc'}, take: 5})
-  .then(msgs => console.log(msgs.map(m => ({ct: m.ciphertext, ct_type: m.cipher_type}))))
-  .finally(() => prisma.$disconnect());
